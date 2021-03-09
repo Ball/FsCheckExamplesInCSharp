1a498ac931105cda3313dfb3ee3bc62c741c5bbf
Brian Ball <bjball@sep.com>
2021-03-09 10:08:47 -0500

    Added a JavaScript Example

---
+++ JavaScript Example/spec/example.spec.ts
@@ -0,0 +1,34 @@
+// import fast-check as fc
+import * as fc from 'fast-check';
+
+module
+function indexOf(text: string, pattern: string): number {
+    return text.indexOf(pattern);
+}
+
+describe("a suite", () => {
+    it("contains a spec with an expectation", () => {
+        expect(true).toBe(true)
+    })
+})
+
+describe('stuff', () =>{
+    it('bad example', () => {
+        const i = indexOf("!",  "!")
+        expect(i).toBe(0)
+    })
+})
+
+describe('property based tests', () => {
+    it('should always contain itself', () =>{
+        fc.assert(fc.property(fc.string(), (text:string) => indexOf(text,text) !== -1));
+    });
+    xit('should always contain substrings', () => {
+        fc.assert(
+            fc.property(fc.string(), fc.string(), fc.string(),
+                (a:string,b:string,c:string) => {
+                    return indexOf(b, a + b + c) !== -1;
+            })
+        )
+    })
+})
---
+++ JavaScript Example/spec/robotname.spec.ts
@@ -0,0 +1,8 @@
+// RT432
+
+// [A-Z]{2}\d{3}
+
+// robot.wipe()
+// oldName != newName
+
+// Any # of robots + any # of wipes and no collisions
